b2a4e55cb5ff646f91f2f968709eb5aebc4ef7e9
xiasin7 <74028123+xiasin7@users.noreply.github.com>
2024-03-02 14:10:17 -0700

    Update TakeAction.py

--- Code/TakeAction.py
+++ Code/TakeAction.py
@@ -1,11 +1,9 @@
-import openai
-import os
+from openai import OpenAI
 
-# openai.api_key = os.getenv('OPEN_API_KEY')
-openai.api_key = "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"
 
 def takeAction(prompt):
-    response = openai.ChatCompletion.create(
+    client = OpenAI(api_key = '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')
+    response = client.Chat.completions.create(
     model="gpt-3.5-turbo",
     messages = prompt,
     temperature = 0.3
